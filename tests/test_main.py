9d813a2730ad1fa8cc8037a250e7a8215a2cc67c
Mike Izbicki <mike@izbicki.me>
2021-03-01 00:43:23 -0800

    update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
-from sorting import _merged, merge_sorted, quick_sorted, cmp_standard, cmp_reverse
+from sorting import _merged, merge_sorted, quick_sorted, cmp_standard, cmp_reverse, cmp_last_digit
 from hypothesis import given
 import hypothesis.strategies as st
 import timeit
+from functools import cmp_to_key
+import copy
 
 
 ints = st.lists(st.integers())
@@ -26,8 +28,19 @@ def test__merge_sorted(str1):
     assert merge_sorted(list(str1)) == sorted(str1)
 
 @given(str1=ints)
+def test__merge_sorted_memory(str1):
+    str1_copy = copy.deepcopy(str1)
+    str1 = list(str1)
+    merge_sorted(str1)
+    assert str1_copy == str1
+
+@given(str1=ints)
 def test__merge_sorted_cmp1(str1):
     assert merge_sorted(list(str1),cmp_reverse) == list(reversed(sorted(str1)))
+
+@given(str1=ints)
+def test__merge_sorted_cmp2(str1):
+    assert merge_sorted(list(str1),cmp_reverse) == list(sorted(str1,key=cmp_to_key(cmp_reverse)))
 
 @given(str1=ints)
 def test__quick_sorted(str1):
@@ -36,3 +49,18 @@ def test__quick_sorted(str1):
 @given(str1=ints)
 def test__quick_sorted_cmp1(str1):
     assert quick_sorted(list(str1),cmp_reverse) == list(reversed(sorted(str1)))
+
+@given(str1=ints)
+def test__quick_sorted_cmp1(str1):
+    assert quick_sorted(list(str1),cmp_reverse) == list(reversed(sorted(str1)))
+
+@given(str1=ints)
+def test__quick_sorted_cmp2(str1):
+    assert quick_sorted(list(str1),cmp_reverse) == list(sorted(str1,key=cmp_to_key(cmp_reverse)))
+
+@given(str1=ints)
+def test__quick_sorted_memory(str1):
+    str1_copy = copy.deepcopy(str1)
+    str1 = list(str1)
+    quick_sorted(str1)
+    assert str1_copy == str1
